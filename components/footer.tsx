1082af0414531b436fae1133e6cf5ca6721f2238
sunanmau5 <sunan.regi@gmail.com>
2024-06-15 18:58:12 +0200

    feat: instagram icon on footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { useMobile } from "@/hooks/use-media";
+
+import { Icons } from "./icons";
+
 function Footer() {
+  const isMobile = useMobile();
+
+  const username = "waworu";
+  const href = isMobile
+    ? `instagram://user?username=${username}`
+    : `https://instagram.com/${username}`;
+
   return (
     <footer className="fixed bottom-0 z-10 w-full bg-white">
-      <h4 className="my-0.5 px-4 py-4 text-center text-sm uppercase sm:px-20">
-        Made with ❤️
-      </h4>
+      <div className="my-0.5 px-4 py-4 sm:px-20 flex justify-between items-center">
+        <a
+          className="flex items-center gap-2"
+          href={href}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Icons.instagram />
+          <h2 className="text-base font-semibold">@{username}</h2>
+        </a>
+      </div>
     </footer>
   );
 }
